f44d8562e0710857030ab0d31a2f5e00b0d99fb5
Kasper T <kasper@webmasteren.eu>
2023-01-14 15:35:29 +0100

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,19 @@
 plugins {
     //https://plugins.gradle.org/plugin/org.jetbrains.intellij
-    id("org.jetbrains.intellij") version "1.3.1"
-    kotlin("jvm") version "1.6.10"
-    java
-    //https://github.com/jeremylong/DependencyCheck
-    id("org.owasp.dependencycheck") version "6.5.3"
+    id("org.jetbrains.intellij") version "1.11.0"
+    kotlin("jvm") version "1.8.0"
+    //https://github.com/jeremylong/DependencyCheck (https://plugins.gradle.org/plugin/org.owasp.dependencycheck)
+    id("org.owasp.dependencycheck") version "7.4.4"
 }
 
 group = "csense-idea"
 version = "0.300"
 
-
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
     updateSinceUntilBuild.set(false)
     plugins.set(listOf("Kotlin", "java"))
-    version.set("2020.3")
+    version.set("2021.3")
 }
 
 repositories {
@@ -25,7 +23,7 @@ repositories {
 }
 
 dependencies {
-    implementation("csense.kotlin:csense-kotlin-jvm:0.0.54")
+    implementation("csense.kotlin:csense-kotlin-jvm:0.0.59")
     implementation("csense.kotlin:csense-kotlin-annotations-jvm:0.0.50")
     implementation("csense.kotlin:csense-kotlin-datastructures-algorithms:0.0.41")
     implementation("csense.idea.base:csense-idea-base:0.1.41")
@@ -36,24 +34,18 @@ tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml
     changeNotes.set(
         """
       <ul>
-        <li>More improvements / exception fixes</li>
-        <li>Coverage now respects suppression</li>
-        <li>Coverage now ignores anonymous classes</li>
-        <li>Gutter icon for navigating to a test</li>
+        <li></li>
       </ul>
       """
     )
 }
 
-
-tasks.getByName("check").dependsOn("dependencyCheckAnalyze")
-
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "11"
     kotlinOptions.freeCompilerArgs = listOf("-progressive")
 }
 
 tasks.withType<JavaCompile> {
-    targetCompatibility = "1.8"
-    sourceCompatibility = "1.8"
+    targetCompatibility = "11"
+    sourceCompatibility = "11"
 }
---
+++ changelog.md
@@ -0,0 +1,4 @@
+# 0.300
+- changed Idea requirement to 2021.3 (which is closely related to the stable version of AS as of writing)
+- tested with Idea 2021 + AS dolphin also in MPP mode
+- 
